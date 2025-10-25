fa2c33fc42221d39e6df1775a5d6212c37249a3c
bharathchandra-gth <bharathlcode18@gmail.com>
2025-10-25 15:06:31 +0530

    Added TMDB KEYS

---
+++ server/controllers/showController.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js";
+import { inngest } from "../inngest/index.js";
+
+// API to get now playing movies from TMDB API
+export const getNowPlayingMovies = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+      }
+    );
+
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: error.message });
+  }
+};
